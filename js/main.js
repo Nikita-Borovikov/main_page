0cf5e10e1c6b51ea65a76b7c3d61701d36daaaf7
fallen <haos.tv1000@gmail.com>
2023-11-13 10:05:07 +0100

    new_branch

--- js/main.js
+++ js/main.js
@@ -5,15 +5,4 @@ $('.menu-btn').on('click',function(e) {
     });
 
 
-    window.onload = function(){
-        window.setInterval(function(){
-        var now = new Date();
-        var clock = document.getElementById("clock");
-        clock.innerHTML = now.toLocaleTimeString();
-        },1000);
-      };
-     
-
-
-
 
